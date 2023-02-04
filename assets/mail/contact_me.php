9ef5b19ed50ae408e245cda256c3883fe15f5aad
Diego da Costa Porto <ddacostaporto@gmail.com>
2023-02-04 18:01:35 +0100

    wip github page design

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,20 +1,19 @@
 <?php
 
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 
 $to = "ddacostaporto@gmail.com";
-$subject = "Website contact form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n";
+$subject = "Github.io contact form:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
+$header = "From: ddacostaporto@gmail.com\n";
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
